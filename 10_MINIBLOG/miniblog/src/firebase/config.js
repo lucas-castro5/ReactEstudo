469bc170ec8b66ff8450fc346b6fe368646777aa
lucascdsantos_caesb <lucascdsantos@caesb.df.gov.br>
2026-08-11 11:45:55 -0300

    Adiantei os reactzada

---
+++ 10_MINIBLOG/miniblog/src/firebase/config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import {getFirestore} from "firebase/firebase"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCS7m-qSCVBTFIhLUSFV_uz1yD5gvVvEJE",
+  authDomain: "miniblog-89d1e.firebaseapp.com",
+  projectId: "miniblog-89d1e",
+  storageBucket: "miniblog-89d1e.firebasestorage.app",
+  messagingSenderId: "108810048391",
+  appId: "1:108810048391:web:094181f6b3239ea8aaf922"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export {db};
