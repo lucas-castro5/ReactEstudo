529ba1f1aad2305f4846e75627697f687602414c
lucascdsantos_caesb <lucascdsantos@caesb.df.gov.br>
2026-08-12 11:46:24 -0300

    projeto mini blog andando

--- 10_MINIBLOG/miniblog/src/firebase/config.js
+++ 10_MINIBLOG/miniblog/src/firebase/config.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import {getFirestore} from "firebase/firebase"
+import {getFirestore} from "firebase/firestore"
 
 const firebaseConfig = {
   apiKey: "AIzaSyCS7m-qSCVBTFIhLUSFV_uz1yD5gvVvEJE",
--- 10_MINIBLOG/miniblog/src/pages/About/About.js
+++ 10_MINIBLOG/miniblog/src/pages/About/About.js
@@ -1,10 +1,20 @@
 // CSS
 import styles from './About.module.css'
+import { Link } from "react-router-dom"
 
 const About = () => {
   return (
-    <div>
-        <h1>About</h1>
+    <div className={styles.about}>
+      <h2>Sobre o Mini <span>Blog</span></h2>
+      <p>
+        O Mini Blog é uma plataforma desenvolvida com React e Firebase que
+        permite aos usuários criar, compartilhar e gerenciar publicações de
+        forma simples e intuitiva. O projeto foi criado com o objetivo de
+        praticar conceitos modernos de desenvolvimento web, como autenticação,
+        gerenciamento de estado, roteamento e integração com banco de dados
+        em tempo real.
+      </p>
+      <Link to="/posts/create" className="btn"> Criar post</Link>
     </div>
   )
 }
