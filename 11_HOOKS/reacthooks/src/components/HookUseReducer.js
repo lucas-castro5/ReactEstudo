014e92917afa9686bbdbed74d54239146611362c
lucascdsantos_caesb <lucascdsantos@caesb.df.gov.br>
2026-08-17 11:39:41 -0300

    Revisando e Aprendendo Hooks

---
+++ 11_HOOKS/reacthooks/src/components/HookUseReducer.js
@@ -0,0 +1,78 @@
+import { useReducer, useState } from "react"
+
+
+const HookUseReducer = () => {
+    // começando com o useReducer
+    const [number, dispatch] = useReducer((state, action) => {
+        return Math.random(state)
+    });
+
+    // avancando no useReducer
+    const initialTasks = [
+        { id: 1, text: "fazer algo" },
+        { id: 2, text: "fazer outro algo" }
+    ]
+
+    const taskReducer = (state, action) => {
+        switch (action.type) {
+            case "ADD":
+                const newTask = {
+                    id: Math.random(),
+                    text: action.text,
+                };
+
+                return [...state, newTask];
+
+            case "DELETE":
+                return state.filter((task) => task.id !== action.id);
+
+            default:
+                return state;
+        }
+    };
+    const [taskText, setTaskText] = useState("")
+    const [tasks, dispatchTasks] = useReducer(taskReducer, initialTasks)
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        dispatchTasks({
+            type: "ADD",
+            text: taskText,
+        });
+
+        setTaskText("");
+    };
+
+
+    const removeTask = (id) => {
+        dispatchTasks({ type: "DELETE", id })
+    }
+    return (
+        <div>
+            <h2>useReducer</h2>
+            <p>Número: {number} </p>
+            <button onClick={dispatch}>Alterar numero</button>
+            <h3>Tarefas: </h3>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    onChange={(e) => setTaskText(e.target.value)}
+                />
+                <input type="submit" value="Enviar" />
+            </form>
+            {tasks.map((task) => (
+                <li
+                    key={task.id}
+                    onDoubleClick={() => removeTask(task.id)}
+                >
+                    {task.text}
+                </li>
+            ))}
+            <hr />
+        </div>
+    )
+}
+
+export default HookUseReducer
---
+++ 11_HOOKS/reacthooks/src/components/HookUseState.js
@@ -0,0 +1,53 @@
+import { useState } from "react"
+
+
+const HookUseState = () => {
+    // 1 - useState
+    let userName = "joao"
+
+    const [name, setName] = useState("Matheus")
+
+    const changeNames = () => {
+        userName = "João Souza"
+
+        setName("Matheus Battisti")
+
+    }
+
+
+    // 2 - useState e input
+    const [age, setAge] = useState(18)
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        // envio a uma API
+        console.log(age)
+    }
+
+    return (
+        <div>
+            {/* 1 - useState */}
+            <h2>UseState</h2>
+            <p>Variável: {userName}</p>
+            <p>useState: {name}</p>
+            <button onClick={changeNames}>Mudar nomes!</button>
+
+            {/* 2 - useState e input */}
+            <p> Digite sua idade: </p>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    value={age}
+                    onChange={(e) => setAge(e.target.value)}
+                />
+                <input type="submit" value="Enviar" />
+            </form>
+            <p>Você tem {age} anos!</p>
+
+            <hr />
+        </div>
+    )
+}
+
+export default HookUseState
